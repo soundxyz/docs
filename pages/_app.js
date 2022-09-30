00739e6049ef3ddc2499b5a8d59afbec4ec5509e
Matt Masurka <m.masurka@gmail.com>
2022-09-30 08:15:04 -0700

    removes files

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import { Prism } from 'prism-react-renderer'
-;(typeof global !== 'undefined' ? global : window).Prism = Prism
-
-require('prismjs/components/prism-graphql')
-require('./global-styles.css')
-
-export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
