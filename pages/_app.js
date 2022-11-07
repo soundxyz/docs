43e8a063a0f2a79214b54cf4d85181fc2868f6a4
Matt Masurka <m.masurka@gmail.com>
2022-11-07 14:25:26 -0800

    New header

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useRouter } from 'next/router'
 import { Prism } from 'prism-react-renderer'
 ;(typeof global !== 'undefined' ? global : window).Prism = Prism
 
@@ -7,6 +7,23 @@ require('prismjs/components/prism-graphql')
 require('nextra-theme-docs/style.css')
 require('../styles/globals.css')
 
+const header = document.querySelector('.nextra-nav-container > .nextra-nav-container-blur')
+
 export default function Nextra({ Component, pageProps }) {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    // CSS overrides for landing page
+    if (router.pathname === '/') {
+      if (header) {
+        header.setAttribute('style', 'background-color: black !important')
+      }
+    } else {
+      if (header) {
+        header.style.backgroundColor = ''
+      }
+    }
+  }, [router.pathname])
+
   return <Component {...pageProps} />
 }
