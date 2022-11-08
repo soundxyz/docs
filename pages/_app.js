0bd04f8ebc017e09ebc4a2532242569a60f0464c
Matt Masurka <m.masurka@gmail.com>
2022-11-08 10:56:09 -0800

    stitches

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import 'tailwindcss/tailwind.css'
-
+import { globalStyles } from '../stitches.config.ts'
 import { Prism } from 'prism-react-renderer'
 ;(typeof global !== 'undefined' ? global : window).Prism = Prism
 
 require('prismjs/components/prism-graphql')
 require('nextra-theme-docs/style.css')
 require('../styles/globals.css')
+globalStyles()
 
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
