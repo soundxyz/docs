3bd7c5e258e4721f1f5fbc3d6e7be77007dce116
Matt Masurka <m.masurka@gmail.com>
2022-11-08 16:30:59 -0800

    LinkCard section

--- components/LandingPageComponent.tsx
+++ components/LandingPageComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { WrappedImage } from '../components/WrappedImage'
 import { styled } from '../stitches.config'
+import { LinkCard } from './LinkCard'
 
 const BigH1 = styled('h1', {
   textAlign: 'center',
@@ -40,48 +41,64 @@ const FlexContainer = styled('div', {
   },
 })
 
+const MiddleLinks = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  '@tabletLandscape': {
+    flexDirection: 'row',
+  },
+})
+
 export const LandingPageComponent = () => {
   return (
-    <Container>
-      <FlexContainer>
+    <>
+      <Container>
+        <FlexContainer>
+          <WrappedImage
+            css={{
+              left: '10%',
+              top: 20,
+              position: 'relative',
+              width: 84,
+              height: 'auto',
+              '@tablet': { top: 0, width: 140 },
+            }}
+            src="/images/hero-small-grid.png"
+            width={84}
+            height={63}
+            layout="responsive"
+          />
+          <BigH1 className="pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+            Tools for empowering artists & collectors
+          </BigH1>
+        </FlexContainer>
         <WrappedImage
-          css={{
-            left: '10%',
-            top: 20,
-            position: 'relative',
-            width: 84,
-            height: 'auto',
-            '@tablet': { top: 0, width: 140 },
-          }}
-          src="/images/hero-small-grid.png"
-          width={84}
-          height={63}
+          src="/images/hero-mobile-bottom-grid.png"
+          width={359}
+          height={331}
           layout="responsive"
+          css={{ width: '90%', maxWidth: 500, '@tablet': { display: 'none' } }}
         />
-        <BigH1 className="pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
-          Tools for empowering artists & collectors
-        </BigH1>
-      </FlexContainer>
-      <WrappedImage
-        src="/images/hero-mobile-bottom-grid.png"
-        width={359}
-        height={331}
-        layout="responsive"
-        css={{ width: '90%', maxWidth: 500, '@tablet': { display: 'none' } }}
-      />
-      <WrappedImage
-        src="/images/hero-desktop-right-grid.png"
-        width={804}
-        height={483}
-        layout="responsive"
-        css={{
-          alignSelf: 'end',
-          marginLeft: 'auto',
-          width: '100%',
-          display: 'none',
-          '@tablet': { display: 'block' },
-        }}
-      />
-    </Container>
+        <WrappedImage
+          src="/images/hero-desktop-right-grid.png"
+          width={804}
+          height={483}
+          layout="responsive"
+          css={{
+            alignSelf: 'end',
+            marginLeft: 'auto',
+            width: '100%',
+            display: 'none',
+            '@tablet': { display: 'block' },
+          }}
+        />
+      </Container>
+      <MiddleLinks>
+        <LinkCard type="protocol" />
+        <LinkCard type="sdk" />
+        <LinkCard type="api" />
+      </MiddleLinks>
+    </>
   )
 }
---
+++ components/LinkCard.tsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { styled } from '../stitches.config'
+import { SoundProtocolIcon, SoundSDKIcon, SoundAPIIcon } from './icons'
+import Link from 'next/link'
+
+const content = {
+  protocol: {
+    heading: 'Sound Protocol',
+    description:
+      'The Sound Protocol is a permissionless, open-source, modular smart contract framework for musicians and creators.',
+    icon: SoundProtocolIcon,
+    path: '/protocol/overview',
+    color: '$iconProtocol',
+  },
+  sdk: {
+    heading: 'Sound SDK',
+    description: 'The Sound SDK is a Typescript library for building applications on the Sound Protocol.',
+    icon: SoundSDKIcon,
+    path: '/sdk',
+    color: '$iconSDK',
+  },
+  api: {
+    heading: 'Sound API',
+    description:
+      'Our official GraphQL API serves indexed event data from Sound Protocol, including metadata like image and audio URLs.',
+    icon: SoundAPIIcon,
+    path: '/sound-api',
+    color: '$iconAPI',
+  },
+}
+
+export const LinkCard = ({ type }: { type: 'protocol' | 'sdk' | 'api' }) => {
+  const currentContent = content[type]
+  return (
+    <OuterContainer>
+      <Link href={currentContent.path} legacyBehavior>
+        <a>
+          <InnerContainer
+            css={{
+              '&:hover > div': {
+                borderColor: currentContent.color,
+              },
+            }}
+          >
+            <IconContainer>{currentContent.icon()}</IconContainer>
+            <Heading>{currentContent.heading}</Heading>
+            <Description>{currentContent.description}</Description>
+          </InnerContainer>
+        </a>
+      </Link>
+    </OuterContainer>
+  )
+}
+
+const OuterContainer = styled('div', {
+  backgroundColor: '$white',
+  color: '$black',
+  width: '100%',
+  height: 295,
+  display: 'flex',
+  justifyContent: 'center',
+  borderRadius: 0,
+
+  '@tabletLandscape': {
+    width: '33.333%',
+    height: 410,
+  },
+})
+
+const InnerContainer = styled('div', {
+  paddingTop: 50,
+  maxWidth: 330,
+  margin: '0 auto',
+
+  svg: {
+    opacity: 0.9,
+  },
+
+  '&:hover': {
+    svg: {
+      opacity: 1,
+    },
+  },
+})
+
+const Heading = styled('h2', {
+  fontSize: 34,
+  fontWeight: 500,
+  marginTop: 32,
+  color: '$neutral900',
+})
+
+const Description = styled('p', {
+  marginTop: 16,
+  color: '$neutral700',
+})
+
+const IconContainer = styled('div', {
+  backgroundColor: '$black',
+  width: 100,
+  height: 100,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 8,
+  borderWidth: 2,
+  borderColor: '$black',
+  transition: 'all 0.2s ease-in-out',
+})
